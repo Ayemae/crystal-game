71a9ff956e7f1ec6610df99cd41887bd5a0b31f1
Alyssa Alecci <Alyssa@Alyssas-iMac.local>
2018-06-17 17:52:05 -0400

    Progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,3 @@
-var goalNumber = 0;
-var playerScore = 0;
-
-
-var touch1 = $("#touch-1");
-var touch1value = 0;
-
-var touch2 = $("#touch-2");
-var touch2value = 0;
-
-var touch3 = $("#touch-3");
-var touch3value = 0;
-
-var touch4 = $("#touch-4");
-var touch4value = 0;
-
-var touchValues = [touch1value, touch2value, touch3value, touch4value];
-var uniqueTouchValues = [];
-
-
-var winCount = 0;
-var lossCount = 0;
-
-
-function getTouchValues() {
-    for (var i = 0; i < touchValues.length; i++) {
-        touchValues[i] = Math.floor(Math.random() * 12 + 1);
-    }
-    $.each(touchValues, function (i, el) {
-        if ($.inArray(el, uniqueTouchValues) === -1) uniqueTouchValues.push(el);
-    });
-    if (uniqueTouchValues.length == 4) {
-    return uniqueTouchValues;}
-    else {
-        return;
-    }
-};
-
-    
-
-
-// Game
-
 $(document).ready(function () {
 
     // function resetGame() {
@@ -52,19 +9,127 @@ $(document).ready(function () {
     //     touch4value = 0;
     // };
 
-    //function startNewGame() {
+
+    //Globals 
+    var playerScore = 0;
+    //var goalNumber is defined in-game
+
+    var touch1value = 0;
+    var touch2value = 0;
+    var touch3value = 0;
+    var touch4value = 0;
+
+    var touchValues = [touch1value, touch2value, touch3value, touch4value];
+    var uniqueTouchValues = []; //resurrect later?
+
+
+    var winCount = 0;
+    var lossCount = 0;
+    var isGameOver = false;
+
+
+
+//Game + Functions
+
+    function getTouchValues() {
+        for (var i = 0; i < touchValues.length; i++) {
+            touchValues[i] = Math.floor(Math.random() * 12 + 1);
+        }
+        return touchValues;
+    };
+    // PseudeCode Amibitions for Touch Values:
+    // There must be AT LEAST ONE ODD NUMBER
+    // There must be AT LEAST ONE EVEN NUMBER
+    // All values MUST BE UNIQUE
+    // Otherwise, re-randomize touch values
+
+
+
+    
+    //define Values For Game
     var goalNumber = Math.floor(Math.random() * 101 + 19);
     getTouchValues();
 
 
-    //};
-    //resetGame();
-    //startNewGame();
+
+    function checkForGameOver() {
+        if (playerScore === goalNumber) {
+            winCount++;
+            isGameOver = true;
+            console.log("You won!");
+        }
+        if (playerScore > goalNumber) {
+            lossCount++;
+            isGameOver = true;
+            console.log("You lost!");
+        }
+        console.log(isGameOver);
+        return isGameOver;
+    };
+
+    function ammendStats() {
+        $("#score").html(playerScore);
+        $("#wins").html("<p>Wins: " + winCount + "</p>");
+        $("#losses").html("<p>Losses: " + lossCount + "</p>");
+    };
+
+
+
+
+
+    // Buttons
+
+    $("#touch-0").on("click", function () {
+        if (isGameOver === false) {
+            playerScore = playerScore + touchValues[0];
+            ammendStats();
+            checkForGameOver();
+        }
+    });
+
+    $("#touch-1").on("click", function () {
+        if (isGameOver === false) {
+            playerScore = playerScore + touchValues[1];
+            checkForGameOver();
+            ammendStats();
+        }
+    });
+
+    $("#touch-2").on("click", function () {
+        if (isGameOver === false) {
+            playerScore = playerScore + touchValues[2];
+            checkForGameOver();
+            ammendStats();
+        }
+    });
+
+    $("#touch-3").on("click", function () {
+        if (isGameOver === false) {
+            playerScore = playerScore + touchValues[3];
+            checkForGameOver();
+            ammendStats();
+        }
+    });
+
+
 
     //test
-    console.log(goalNumber);
+    console.log(touchValues);
 
-    console.log(uniqueTouchValues);
+
+
+    //print to page at game start
+    $("#goal-no").html(goalNumber);
+    $("#score").html(playerScore);
+    $("#wins").html("<p>Wins: " + winCount + "</p>");
+    $("#losses").html("<p>Losses: " + lossCount + "</p>");
+
+
+
+
+
+
+
 
 
 
@@ -73,7 +138,10 @@ $(document).ready(function () {
 
 
 
-
-goalNumber = $("#goal-no");
-playerScore = $("#score");
+// Welp, that was a waste of time.
+        // for (var t = 0; t < 4; t++) {
+        //     $('#touch-' + t).on("click", function () {
+        //     playerScore = playerScore + touchValues[t];
+        //     console.log(playerScore);
+        // });
 
